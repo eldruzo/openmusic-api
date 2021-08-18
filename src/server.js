6296cb47b304676506b2777979ec2f2e83fb799f
Achmad Nabil <nabileldruzo@gmail.com>
2021-08-18 09:22:34 +0700

    added songs plugin to hapi config

--- src/server.js
+++ src/server.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 
+const songs = require('./api/songs');
+const SongsService = require('./services/postgres/SongService');
+const SongsValidator = require('./validator/songs');
+
 const init = async () => {
+  const songsService = new SongsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -11,6 +17,14 @@ const init = async () => {
       cors: {
         origin: ['*'],
       },
+    },
+  });
+
+  await server.register({
+    plugin: songs,
+    options: {
+      service: songsService,
+      validator: SongsValidator,
     },
   });
 
